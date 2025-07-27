da1baa6062833f959aea7fa0962f6eb75a706bd6
Sergio Murillo <samb300593@gmail.com>
2025-07-26 18:21:35 -0700

    Rebuild navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
 
         <nav className='fixed top-0 w-full bg-white shadow z-50'>
             <ul className='flex justify-center gap-8 p-4 text-gray-700 font-medium'>
-                <li><a href="#inicio">img</a></li>
+                <li><a href="#inicio">Inicio</a></li>
                 <li><a href="#experiencia">Experiencia</a></li>
                 <li><a href="#servicios">Servicios</a></li>
                 <li><a href="#contacto">Contacto</a></li>
